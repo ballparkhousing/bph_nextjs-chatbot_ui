077690b0e9834d602a6d19ea064b742a6d34d963
Nobinwanne <ndidiobinwanne@gmail.com>
2024-06-10 01:00:12 -0600

    revised content

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -36,24 +36,14 @@ export function ChatPanel({
 
   const exampleMessages = [
     {
-      heading: 'What are the',
-      subheading: 'trending memecoins today?',
-      message: `What are the trending memecoins today?`
+      heading: 'I want to Rent',
+      subheading: 'Click here if you are a Renter',
+      //message: `I would like to buy 42 $DOGE`
     },
     {
-      heading: 'What is the price of',
-      subheading: '$DOGE right now?',
-      message: 'What is the price of $DOGE right now?'
-    },
-    {
-      heading: 'I would like to buy',
-      subheading: '42 $DOGE',
-      message: `I would like to buy 42 $DOGE`
-    },
-    {
-      heading: 'What are some',
-      subheading: `recent events about $DOGE?`,
-      message: `What are some recent events about $DOGE?`
+      heading: 'I want to Build',
+      subheading: `Click here if you are a Developer`,
+      //message: `What are some recent events about $DOGE?`
     }
   ]
 
